2026cc1ce7f46aaca29a8818dbce82b9c514e848
Keenan Samway <70533057+keenansamway@users.noreply.github.com>
2022-11-11 16:43:35 -0500

    Update .gitignore

--- CNN-LSTM/get_loader.py
+++ CNN-LSTM/get_loader.py
@@ -1,13 +1,50 @@
 import os
 import pandas as pd
-
+import spacy
 import torch
 from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 from PIL import Image
 from torchvision.transforms import transforms
 
-# class Vocabulary:
+# https://spacy.io/usage/linguistic-features#tokenization
+# https://spacy.io/api/tokenizer
+spacy_eng = spacy.load("en_core_web_sm")
 
+# class Vocabulary:
+class Vocabulary:
+    def __init__(self, freq_threshold):
+        self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
+        self.stoi = {"<PAD>": 0, "<SOS>": 1, "<EOS>": 2, "<UNK>": 3}
+        self.freq_threshold = freq_threshold
+        
+    def __len__(self):
+        return len(self.itos)
+    
+    @staticmethod
+    def tokenizer_eng(text):
+        return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
+    
+    def build_vocabulary(self, sentence_list):
+        frequencies = {}
+        idx = 4
+        
+        for sentence in sentence_list:
+            for word in self.tokenizer_eng(sentence):
+                if word not in frequencies:
+                    frequencies[word] = 1
+                else:
+                    frequencies[word] += 1
+                    
+                if frequencies[word] == self.freq_threshold:
+                    self.stoi[word] = idx
+                    self.itos[idx] = word
+                    idx += 1
+                    
+    def numericalize(self, text):
+        tokenized_text = self.tokenizer_eng(text)
+        
+        return [self.stoi[token] if token in self.stoi else self.stoi["<UNK>"] for token in tokenized_text]
 
 # class PCCD(Dataset):
 
