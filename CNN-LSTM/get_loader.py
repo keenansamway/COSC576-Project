1dede8f188881b1833768029354cbccce7dd1549
Keenan Samway <70533057+keenansamway@users.noreply.github.com>
2022-11-11 22:05:26 -0500

    Complete get_loader

--- CNN-LSTM/get_loader.py
+++ CNN-LSTM/get_loader.py
@@ -2,9 +2,9 @@ import os
 import pandas as pd
 import spacy
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torch.nn.utils.rnn import pad_sequence
-from PIL import Image
 from torchvision.transforms import transforms
 
 # https://spacy.io/usage/linguistic-features#tokenization
@@ -47,13 +47,82 @@ class Vocabulary:
         return [self.stoi[token] if token in self.stoi else self.stoi["<UNK>"] for token in tokenized_text]
 
 # class PCCD(Dataset):
+class PCCD(Dataset):
+    def __init__(self, root_dir, captions_file, transform=None, freq_threshold=5):
+        self.root_dir = root_dir
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+        
+        self.df["general_impression"] = self.df["general_impression"].fillna("")
+        
+        # Get img, caption columns
+        self.imgs = self.df["title"]
+        self.captions = self.df["general_impression"]
 
+        # Initialize and build vocabulary
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocabulary(self.captions.tolist())
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, index):
+        caption = self.captions[index]
+        img_id = self.imgs[index]
+        img = Image.open(os.path.join(self.root_dir, img_id)).convert("RGB")
+        
+        if self.transform is not None:
+            img = self.transform(img)
+            
+        numericalized_caption = [self.vocab.stoi["<SOS>"]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<EOS>"])
+        
+        return img, torch.tensor(numericalized_caption)
+    
 
 # class MyCollate:
+class MyCollate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+        
+    def __call__(self, batch):
+        imgs = [item[0].unsqueeze(0) for item in batch]
+        imgs = torch.cat(imgs, dim=0)
+        targets = [item[1] for item in batch]
+        targets = pad_sequence(targets, batch_first=False, padding_value=self.pad_idx)
+        
+        return imgs, targets
 
 
-# def get_loader(root_folder, )
+# def get_loader()
+def get_loader(root_folder, annotation_file, transform, batch_size=32, num_workers=8, shuffle=True, pin_memory=True):
+    
+    dataset = PCCD(root_folder, annotation_file, transform=transform)
+    
+    pad_idx = dataset.vocab.stoi["<PAD>"]
+    
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=shuffle,
+        pin_memory=pin_memory,
+        collate_fn=MyCollate(pad_idx=pad_idx),
+    )
+    
+    return loader, dataset
 
 
 # if __name__ == "__main__":
+if __name__ == "__main__":
+    transform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()])
+    
+    images_dir = "datasets/PCCD/images/full"
+    captions_file = "datasets/PCCD/raw.csv"
 
+    loader, dataset = get_loader(images_dir, captions_file, transform=transform)
+   
+    for idx, (imgs, captions) in enumerate(loader):
+        print(imgs.shape)
+        print(captions.shape)
