b0018a19a3c12c0ea6588a81d86f09bd23b02953
Keenan Samway <70533057+keenansamway@users.noreply.github.com>
2022-11-11 22:05:57 -0500

    Add caption_image function

--- CNN-LSTM/cnn-lstm_model.py
+++ CNN-LSTM/cnn-lstm_model.py
@@ -51,4 +51,21 @@ class CNNtoLSTM(nn.Module):
         outputs = self.decoder(features, captions)
         return outputs
     
+    def caption_image(self, image, vocabulary, max_length=50):
+        result_caption = []
         
+        with torch.no_grad():
+            x = self.encoder(image).unsqueeze(0)
+            states = None
+            
+            for _ in range(max_length):
+                hiddens, states = self.decoder.lstm(x, states)
+                output = self.decoder.linear(hiddens.squeeze(0))
+                predicted = output.argmax(dim=0)
+                
+                result_caption.append(predicted.item())
+                x = self.decoder.embed(predicted).unsqueeze(0)
+                
+                if vocabulary.itos[predicted.item()] == "<EOS>":
+                    break
+        return [vocabulary.itos[idx] for idx in result_caption]
