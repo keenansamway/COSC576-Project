da1b64af6f7b4cb424ee4d8ee6130c9a660d89e9
Keenan Samway <70533057+keenansamway@users.noreply.github.com>
2022-12-08 10:54:45 -0500

    Add CLIP-ViT example

---
+++ Transformer-Model/clip_example.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = processor(images=image, return_tensors="pt")
+
+image_features = model.get_image_features(**inputs)
+
+print(image_features.size())
